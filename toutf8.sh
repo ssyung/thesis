c840df6bb77961e758a47af8c580b98e67ce1c97
stephervescent <steph@stephervescents-MacBook-Air.local>
2019-04-17 19:37:13 -0400

    fix issue where i was accidentally overwriting files making them empty.

--- toutf8.sh
+++ toutf8.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-cd txt;
+mv txt txtconv;
+mkdir txt;
+cd txtconv;
 dos2unix *;
 for i in *.txt; do
   if file --mime-encoding -b $i | grep 'utf-8'; then 
     echo "$i is already good";
+    cp $i ../txt/;
   elif file --mime-encoding -b $i | grep 'unknown-8bit'; then 
-    iconv -f WINDOWS-1252 -t UTF-8 $i > $i;
+    iconv -f WINDOWS-1252 -t UTF-8 $i > ../txt/$i;
   elif file --mime-encoding -b $i | grep 'iso-8859-1'; then 
-    iconv -f ISO-8859-1 -t UTF-8 $i > $i;
+    iconv -f ISO-8859-1 -t UTF-8 $i > ../txt/$i;
   elif file --mime-encoding -b $i | grep 'us-ascii'; then 
-    iconv -f US-ASCII -t UTF-8 $i > $i;
+    iconv -f US-ASCII -t UTF-8 $i > ../txt/$i;
   else
-    cp $i unknown/;
+    cp $i ../unknowntxt/;
   fi
 done;
 
