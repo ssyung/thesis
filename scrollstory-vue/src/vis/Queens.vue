7bcdf2185203e092fccc178811d67a02588fe3d4
stephervescent <steph@stephervescents-MacBook-Air.local>
2019-05-02 07:58:48 -0400

    add audio file to Queens slide.

--- scrollstory-vue/src/vis/Queens.vue
+++ scrollstory-vue/src/vis/Queens.vue
@@ -3,6 +3,7 @@
     <div class="qns-wrap">
       <h1>Queens</h1>
       <p>Queens Memory is an ongoing program supported by Queens Library and Queens College, designed to collect stories, images and other evidence of life in the borough of Queens... Queens Memory empowers residents from diverse backgrounds to document the personal histories that together tell a more complete story of life in the borough.</p>
+      <audio controls src="/static/QMP_clips_mixdown.mp3"></audio>
     </div>
   </div>
 </template>
